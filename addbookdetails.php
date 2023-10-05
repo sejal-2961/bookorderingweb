aa21fae4c3df90e9ab6e2235b2621cc524fe71d1
sejal-2961 <sejal2961@gmail.com>
2023-10-05 12:58:52 +0530

    commit the changes in code

---
+++ addbookdetails.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add details</title>
+    <link rel="stylesheet" href="homestyle.css">
+</head>
+<body>
+    <div class="nav_bar">
+    <header>
+        <img src="image/logo.png" alt="logo" class="logo">
+        <nav>
+            <ul class="nav_links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="addbookdetails.php">Add Book Details</a></li>
+                <li><a href="bookordering.php">Book Ordering</a></li>
+                <li><a href="receiptgeneration.php">Receipt</a></li>
+                <li><a href="bookinghistory.php">Book History</a></li>
+            </ul>
+        </nav>
+    </header>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="homestyle.css">
+</head>
+<body>
+    <div class="nav_bar">
+    <header>
+        <img src="image/logo.png" alt="logo" class="logo">
+        <nav>
+            <ul class="nav_links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="addbookdetails.php">Add Book Details</a></li>
+                <li><a href="bookordering.php">Book Ordering</a></li>
+                <li><a href="receiptgeneration.php">Receipt</a></li>
+                <li><a href="bookinghistory.php">Book History</a></li>
+            </ul>
+        </nav>
+    </header>
+    </div>
+    <!-- BAckgroung image -->
+    <!-- <img src="image/bg.png" > -->
+
+
+
+        <div class="main-header">
+            <div class="header">
+                <h5>Welcome to Book Store</h5>
+                <h2>For All Your Reading Needs</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos laudantium hic consequatur eaque. </p>
+                <a href="addbookdetails.php" class="btn">Add books</a>
+            </div>
+        </div>
+
+    <div class="image">
+        <img src="" alt="Bookimages" height="400px" width="350px">
+    </div>
+
+
+    
+</body>
+</html>
